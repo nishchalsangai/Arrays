c26b90af5a755f9834be678527adb19d0672008d
nishchalsangai <nishchalsangai@gmail.com>
2021-06-10 19:16:15 +0530

    Left Rotate an array by d times where d can be any natural number

--- leftRotateArrayByD.cpp
+++ leftRotateArrayByD.cpp
@@ -1,8 +1,37 @@
 /*In this program I will code all the three methods that can help you solve this   
+call this function number of times you want to rotate using for loop (TC:-O(nd))
+void leftRotateEverySingleTime(int arr[],int n)
+{
+    int temp=arr[0];
+    for(int i=1;i<n;i++)
+    {
+        arr[i-1]=arr[i];
+    }
+    arr[n-1]=temp;
+}
+it's better than calling every single time (TC:-O(n+d),SC:-O(d))
+void leftRotateArrayMethodII(int arr[],int n,int d)
+{
+    int temp[d];
+    for(int i=0;i<d;i++)
+    {
+        temp[i]=arr[i];
+    }
+    for(int i=d;i<n;i++)
+    {
+        arr[i-d]=arr[i];
+    }
+    for(int i=0;i<d;i++)
+    {
+        arr[n-d+i]=temp[i];
+    }
+
+}
  */
 
 #include<bits/stdc++.h>
 using namespace std;
+
 void reverse(int arr[],int low,int high)
 {
     while(low<high)
